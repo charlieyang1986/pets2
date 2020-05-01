c08de27fed3d9f0f35eb2067ad5843e4a9b6bd24
Dmytro Boichuk <dboichuk@mail.greenriver.edu>
2020-05-01 16:35:40 -0400

    changed index and home.html

--- index.php
+++ index.php
@@ -12,12 +12,14 @@ $f3 = Base::instance();
 
 //Default route
 $f3->route('GET /', function() {
-    echo '<h1>My Pets<h1>';
-
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
+$f3->route('GET /order', function() {
+    $view = new Template();
+    echo $view->render('views/order.html');
+});
 
 
 //Run F3
