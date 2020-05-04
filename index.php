1649bb011f075432ad2e38e646435a37f0c8896a
Aaron Tadina <atadina@mail.greenriver.edu>
2020-05-04 17:59:36 -0400

    modified index page to validate inputs

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -16,12 +16,20 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET /order', function() {
+$f3->route('GET|POST /order', function($f3) {
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        if(empty($_POST['petkind'])){
+            echo "Please supply a pet type";
+        }
+    }
+    else{
+        $_SESSION['pet']=$_POST['petkind'];
+        $f3->reroute("summary");
+    }
     $view = new Template();
     echo $view->render('views/pet-order.html');
 
 });
-
 
 //Run F3
 $f3->run();
